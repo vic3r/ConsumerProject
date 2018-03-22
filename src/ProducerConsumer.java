e9e5eca7c9087ade1b11b78edd6f03642bde5185
vic3r <barcelonamane@hotmail.com>
2018-03-21 21:09:33 -0600

    Add Components JPanel

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -2,6 +2,7 @@
 import java.awt.Color;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 /*
@@ -14,24 +15,53 @@ import javax.swing.JPanel;
  *
  */
 public class ProducerConsumer {
-
+    
+    private JFrame frame;
+    private JPanel panel;
+    private JButton btProduce;
+    private JButton btConsume;
+    private JLabel produceLabel;
+    private JLabel consumeLabel;
+    
     public ProducerConsumer() {
-        JFrame f = new JFrame("Producer Consumer");    
-        JPanel panel = new JPanel();  
-        panel.setBounds(0,0,600,800);    
-        panel.setBackground(Color.gray);  
-        JButton btProduce = new JButton("Produce");     
-        btProduce.setBounds(50,100,80,30);    
-        btProduce.setBackground(Color.blue);   
-        JButton btConsume = new JButton("Consume");   
-        btConsume.setBounds(100,100,80,30);    
-        btConsume.setBackground(Color.green);   
-        panel.add(btProduce); panel.add(btConsume);  
-        f.add(panel);  
-        f.setSize(600,800);    
-        f.setLayout(null);    
-        f.setVisible(true);
+        createComponents();
+        setComponentsFeatures();
+        addComponents();
+        setFrameFeatures();      
+
     }
+    
+    public void createComponents() {
+        frame = new JFrame("Producer Consumer");    
+        panel = new JPanel();  
+        btProduce = new JButton("Produce"); 
+        btConsume = new JButton("Consume");
+        produceLabel = new JLabel();
+        consumeLabel = new JLabel();
+    }
+    
+    public void setComponentsFeatures() {
+        panel.setBounds(0,0,500,500);    
+        panel.setBackground(Color.gray); 
+        btProduce.setBounds(50,100,80,30);   
+        btProduce.setBackground(Color.blue); 
+        produceLabel.setBounds(50, 150, 50, 50);
+        btConsume.setBounds(100,100,80,30);  
+        consumeLabel.setBounds(100, 150, 50 , 50);
+        btConsume.setBackground(Color.green);
+    }
+    
+    public void addComponents(){
+        this.panel.add(btProduce); 
+        this.panel.add(btConsume); 
+        this.frame.add(panel);
+    }
+    
+    public void setFrameFeatures() {
+        frame.setSize(500,500);    
+        frame.setLayout(null);    
+        frame.setVisible(true);
+    }  
     
     public static void main(String[] args) {
         Buffer buffer = new Buffer();
