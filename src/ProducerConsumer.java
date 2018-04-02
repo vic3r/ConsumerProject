01ed73c6941a309007f6df2cef70c27426a4e7f3
vic3r <barcelonamane@hotmail.com>
2018-04-02 02:46:11 -0500

    Reorder some components

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -61,21 +61,24 @@ public final class ProducerConsumer {
         waitingTime = new JLabel("Waiting Time");
         inputBufferSize = new JTextField();
         inputWaitingTimeProducers = new JTextField();
+        inputWaitingTimeConsumers = new JTextField();
     }
     
     public void setComponentsFeatures() {
         panel.setBounds(0,0,500,500);
         panel.setBackground(Color.gray); 
-        btProduce.setBounds(50,100,80,30);   
+        btProduce.setBounds(50, 50,80,30);   
         btProduce.setBackground(Color.blue); 
-        produceLabel.setBounds(20, 100, 100, 50);
-        btConsume.setBounds(100,100,80,30);  
-        consumeLabel.setBounds(20, 150, 100 , 50);
+        btConsume.setBounds(150,50,120,30);  
         btConsume.setBackground(Color.green);
+        
+        produceLabel.setBounds(20, 50, 100, 50);
+        consumeLabel.setBounds(20, 150, 100 , 50);
         bufferSize.setBounds(20, 200, 100, 50);
         rangeBufferSize.setBounds(20, 250, 120, 50);
-        inputBufferSize.setBounds(120, 220, 50, 30);
-        inputWaitingTimeProducers.setBounds(200, 120, 50, 30);
+        inputBufferSize.setBounds(120, 210, 50, 30);
+        inputWaitingTimeProducers.setBounds(200, 105, 50, 30);
+        inputWaitingTimeConsumers.setBounds(200, 155, 50, 30);
         startProducerConsumer.setBounds(100, 300, 100, 130);
         
     }
@@ -91,7 +94,9 @@ public final class ProducerConsumer {
         this.panel.add(inputBufferSize);
         this.panel.add(inputWaitingTimeProducers);
         this.panel.add(startProducerConsumer);
-        
+        this.panel.add(inputWaitingTimeConsumers);
+        this.panel.add(waitingTime);
+        this.panel.add(bufferQuantity);
         this.frame.add(panel);
     }
     
