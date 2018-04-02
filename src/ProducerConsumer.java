be5b4335f6bb29e2d20e603eab8401d6b539c113
Victor Garcia <barcelonamane@hotmail.com>
2018-04-02 02:47:14 -0500

    Feat/reorder components (#5)
    * Set layout as relative
    
    * Add labels needed
    
    * Add labels for waiting and buffer
    
    * Add buffer size and producer to the view
    
    * Improvements to the components layout
    
    * Reorder some components

--- src/ProducerConsumer.java
+++ src/ProducerConsumer.java
@@ -1,9 +1,14 @@
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.LayoutManager;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -11,14 +16,28 @@ import javax.swing.JPanel;
  * and open the template in the editor.
  */
 
-public class ProducerConsumer {
+public final class ProducerConsumer {
     
     private JFrame frame;
     private JPanel panel;
+    
+    //Buttons
     private JButton btProduce;
     private JButton btConsume;
+    private JButton startProducerConsumer;
+    
+    //JLabels
     private JLabel produceLabel;
     private JLabel consumeLabel;
+    private JLabel bufferSize;
+    private JLabel rangeBufferSize;
+    private JLabel waitingTime;
+    private JLabel bufferQuantity;
+ 
+    //JTextFields
+    private JTextField inputBufferSize;
+    private JTextField inputWaitingTimeProducers;
+    private JTextField inputWaitingTimeConsumers;
     
     public ProducerConsumer() {
         createComponents();
@@ -33,44 +52,71 @@ public class ProducerConsumer {
         panel = new JPanel();  
         btProduce = new JButton("Producer"); 
         btConsume = new JButton("Consumer");
-        produceLabel = new JLabel();
-        consumeLabel = new JLabel();
+        startProducerConsumer = new JButton("Start");
+        produceLabel = new JLabel("Producers");
+        consumeLabel = new JLabel("Consumers");
+        bufferSize = new JLabel("Buffer size");
+        rangeBufferSize = new JLabel("Range buffer size");
+        bufferQuantity = new JLabel("Quantity");
+        waitingTime = new JLabel("Waiting Time");
+        inputBufferSize = new JTextField();
+        inputWaitingTimeProducers = new JTextField();
+        inputWaitingTimeConsumers = new JTextField();
     }
     
     public void setComponentsFeatures() {
-        panel.setBounds(0,0,500,500);    
+        panel.setBounds(0,0,500,500);
         panel.setBackground(Color.gray); 
-        btProduce.setBounds(50,100,80,30);   
+        btProduce.setBounds(50, 50,80,30);   
         btProduce.setBackground(Color.blue); 
-        produceLabel.setBounds(50, 150, 50, 50);
-        btConsume.setBounds(100,100,80,30);  
-        consumeLabel.setBounds(100, 150, 50 , 50);
+        btConsume.setBounds(150,50,120,30);  
         btConsume.setBackground(Color.green);
+        
+        produceLabel.setBounds(20, 50, 100, 50);
+        consumeLabel.setBounds(20, 150, 100 , 50);
+        bufferSize.setBounds(20, 200, 100, 50);
+        rangeBufferSize.setBounds(20, 250, 120, 50);
+        inputBufferSize.setBounds(120, 210, 50, 30);
+        inputWaitingTimeProducers.setBounds(200, 105, 50, 30);
+        inputWaitingTimeConsumers.setBounds(200, 155, 50, 30);
+        startProducerConsumer.setBounds(100, 300, 100, 130);
+        
     }
     
     public void addComponents(){
+        this.panel.setLayout(null);
         this.panel.add(btProduce); 
         this.panel.add(btConsume); 
         this.panel.add(produceLabel);
         this.panel.add(consumeLabel);
+        this.panel.add(bufferSize);
+        this.panel.add(rangeBufferSize);
+        this.panel.add(inputBufferSize);
+        this.panel.add(inputWaitingTimeProducers);
+        this.panel.add(startProducerConsumer);
+        this.panel.add(inputWaitingTimeConsumers);
+        this.panel.add(waitingTime);
+        this.panel.add(bufferQuantity);
         this.frame.add(panel);
     }
     
     public void setFrameFeatures() {
         frame.setSize(500,500);    
-        frame.setLayout(null);    
+        frame.setLayout(null);
         frame.setVisible(true);
+        frame.setResizable(false);
     }  
     
     public static void main(String[] args) {
-        Buffer buffer = new Buffer();
         
         ProducerConsumer produceConsumer = new ProducerConsumer();
+   //     Buffer buffer = new Buffer();
         
-        Producer producer = new Producer(buffer);
-        producer.start();
-        
-        Consumer consumer = new Consumer(buffer);
-        consumer.start();
+//        
+//        Producer producer = new Producer(buffer);
+//        producer.start();
+//        
+//        Consumer consumer = new Consumer(buffer);
+//        consumer.start();
     }
 }
